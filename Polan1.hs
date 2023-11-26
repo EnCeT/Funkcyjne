4e679d23ca606037456d3deb9cb4c1bd67733e80
Encet <kaliszewskigrzegorz.z.o.o@gmail.com>
2023-11-26 03:27:36 +0100

    2

--- Polan1.hs
+++ Polan1.hs
@@ -4,7 +4,7 @@ f 0 0 = 69
 f x y = (x*x + y*y) / 2*x*y
 
 fx :: Float -> Float
-fx x = f x 1  
+fx x = f x 1
 
 --Zad 2
 m3 :: Int -> Int
@@ -13,10 +13,10 @@ m3 x = x * 2
 m2 :: Int -> Int
 m2 x = x * 3
 
-sumaWartości :: (Int -> Int) -> (Int -> Int) -> Int -> Int -> Int 
+sumaWartości :: (Int -> Int) -> (Int -> Int) -> Int -> Int -> Int
 sumaWartości m3 m2 x y = m3 x + m2 y
 
-sumaW :: Int -> Int -> Int 
+sumaW :: Int -> Int -> Int
 sumaW x y = sumaWartości m3 m2 x y
 
 --Zad 3
@@ -38,10 +38,31 @@ iloczynListy [] = 1
 iloczynListy x = head x * iloczynListy (tail x)
 
 --Zad 6
+--funkcja łacząca
 merge :: [Integer] -> [Integer] -> [Integer]
-merge 
+merge xs [] = xs
+merge [] ys = ys
+merge (x:xs) (y:ys) = if x < y then y :merge (x:xs) ys else x :merge xs (y:ys)
+
+--funkcja dzieląca
+d :: [a] -> ([a], [a])
+d xs = splitAt (length xs `div` 2) xs
 
 mergeSort :: [Integer] -> [Integer]
-mergeSort xs = if length xs < 2 then m (splitAt (length `div` 2) xs) else m (splitAt length `div` 2 xs) 
+mergeSort [] = []
+mergeSort [x] = [x]
+mergeSort xs = merge (mergeSort l) (mergeSort r)
               where
-                  m (xs,ys) = (mergeSort xs, mergeSort ys)
+                (l,r) = d xs
+
+--Zad 7
+dzielnik :: Int -> Int -> Bool
+dzielnik x y | y `mod` x == 0 = True
+             | otherwise = False
+
+divList :: Int -> [Int]
+divList y = [x | x <- [1..(y `div` 2)], dzielnik x y == True]
+
+czyDoskonala :: Int -> Bool
+czyDoskonala x = if (sum (divList x)) == x then True else False
+
--- Polan2.hs
+++ Polan2.hs
@@ -1,3 +1,69 @@
+--Zad 1
+liniowa :: Int -> Int
+liniowa x = x
 
-test:: Int -> Int
-test x = 1
+kwadratowa :: Int -> Int
+kwadratowa x = x * x
+
+jestPunktemStalym :: (Eq a) => a -> (a -> a) -> Bool
+jestPunktemStalym x f = if f x == x then True else False
+
+--Zad 2
+ilosum :: (Num a, Show a, Ord a) => a -> a -> String
+ilosum x y | x * y > x + y = "Iloczyn " ++ show x ++ " oraz " ++ show y ++ " jest wiekszy od sumy"
+           | otherwise = "Iloczyn " ++ show x ++ " oraz " ++ show y ++ " nie jest wiekszy od sumy"
+
+--Zad 4
+dzielnik :: Int -> Int -> Bool
+dzielnik x y | y `mod` x == 0 = True
+             | otherwise = False
+
+dzielniki :: Int -> [Int]
+dzielniki y = [x | x <- [1..(y `div` 2)], dzielnik x y == True]
+
+--Zad 5
+suma :: [Int] -> Int
+suma [] = 0
+suma [x] = x + suma []
+suma (x:xs) = x + suma xs
+
+i :: [(Int, Int)] -> Int
+i [] = 0
+i ((x, y):xs) = if x >= 0 && y >= 0 then 1 + i xs else i xs
+
+ii :: [(Int, Int)] -> Int
+ii [] = 0
+ii ((x, y):xs) = if x >= 0 && y <= 0 then 1 + ii xs else ii xs
+
+iii :: [(Int, Int)] -> Int
+iii [] = 0
+iii ((x, y):xs) = if x <= 0 && y <= 0 then 1 + iii xs else iii xs
+
+iv :: [(Int, Int)] -> Int
+iv [] = 0
+iv ((x, y):xs) = if x <= 0 && y >= 0 then 1 + iv xs else iv xs
+
+ktoraCwiartka :: [(Int,Int)] -> Int
+ktoraCwiartka xs | i xs == maximum list = 1
+                 | ii xs == maximum list = 2
+                 | iii xs == maximum list = 3
+                 | otherwise = 4
+                where 
+                    list = [i xs, ii xs, iii xs, iv xs]
+
+
+
+--Zad 6
+podlisty :: [Integer] -> [[Integer]]
+podlisty [] = []
+podlisty xs | n < length xs - 1 = 
+podlisty xs | otherwise = xs : podlisty mniejsza n + 1 hs
+            where 
+                mniejsza n xs = m splitAt n - 1 xs
+                m (xs,ys) = xs ++ drop 0 ys
+-- pokolei usuwać elementy z listy i mergeować a później to samo dla tabeli o 1 mniejszej
+
+--Zad 7
+przeksztalcListe :: (Int -> Int) -> [Int] -> [Int]
+przeksztalcListe f [x] = f x : []
+przeksztalcListe f (x:xs) = f x : przeksztalcListe f xs
