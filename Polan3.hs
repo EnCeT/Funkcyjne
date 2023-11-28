d6e69e8b141ef3e8f1afd365f29cdbf6ed638575
Encet <kaliszewskigrzegorz.z.o.o@gmail.com>
2023-11-28 14:53:11 +0100

    4

--- Polan3.hs
+++ Polan3.hs
@@ -1,5 +1,18 @@
---Zad 1 
-wielkieLitery :: [String] -> [Char]
-wielkieLitery y = if head y `elem` ['A'..'Z'] then [head y] else []
-wielkieLitery (y : ys) =  if  head y `elem` ['A'..'Z'] then head y ++ wielkieLitery ys else wielkieLitery ys
+import Distribution.Simple.Program.HcPkg (list)
 
+--Zad 1 
+--wielkieLitery :: [String] -> [Char]
+--wielkieLitery y = if head y `elem` ['A'..'Z'] then [head y] else []
+--wielkieLitery (y : ys) =  if  head y `elem` ['A'..'Z'] then head y ++ wielkieLitery ys else wielkieLitery ys
+
+--Zad 2
+isItPalindrom :: String -> Int
+isItPalindrom s | s == reverse s = length s
+                | otherwise = 0
+
+dlugoscPalindromow :: [String] -> Int
+dlugoscPalindromow [x] = isItPalindrom x
+dlugoscPalindromow (x:list) = isItPalindrom x + dlugoscPalindromow list
+
+dlugoscPalindromow2 :: [String] -> Int
+dlugoscPalindromow2 list = filter (\x -> length x == length (reverse x)) list
