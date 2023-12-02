460374863896b839fdb9270868235e18f768eef0
Encet <kaliszewskigrzegorz.z.o.o@gmail.com>
2023-12-02 23:01:03 +0100

    5

--- Polan3.hs
+++ Polan3.hs
@@ -1,9 +1,7 @@
-import Distribution.Simple.Program.HcPkg (list)
 
 --Zad 1 
---wielkieLitery :: [String] -> [Char]
---wielkieLitery y = if head y `elem` ['A'..'Z'] then [head y] else []
---wielkieLitery (y : ys) =  if  head y `elem` ['A'..'Z'] then head y ++ wielkieLitery ys else wielkieLitery ys
+maleLitery :: [String] -> [String]
+maleLitery list = map (filter (\x -> elem x  ['a'..'z'])) list
 
 --Zad 2
 isItPalindrom :: String -> Int
@@ -14,5 +12,35 @@ dlugoscPalindromow :: [String] -> Int
 dlugoscPalindromow [x] = isItPalindrom x
 dlugoscPalindromow (x:list) = isItPalindrom x + dlugoscPalindromow list
 
-dlugoscPalindromow2 :: [String] -> Int
-dlugoscPalindromow2 list = filter (\x -> length x == length (reverse x)) list
+--Zad 3
+f:: (Integer,Integer) -> (Integer,Integer)
+f (x,y) = (y,x+y)
+
+
+fib::(Integer,Integer) -> [(Integer,Integer)]
+fib (x,y) = iterate f (x,y)
+
+--Zad 4
+dlugosc:: [a] -> Int
+dlugosc list = sum (map (\x -> 1) list)
+
+--Zad 5
+slowaDlugosc:: Char -> Char -> Integer -> [String]
+slowaDlugosc x y 0 = [""]
+slowaDlugosc d b n = map (d:) (slowaDlugosc d b (n - 1)) ++ map (b:) (slowaDlugosc d b (n - 1))
+
+--Zad 6.
+low :: (Ord a) => [a] -> a -> [a]
+low [x] q = [x]
+low (x:xs) q = if x <= q then [x] ++ low xs q  else low xs q
+
+high ::(Ord a) => [a] -> a -> [a]
+high [x] q = if x > q then [x] else [] 
+high (x:xs) q = if x > q then [x] ++ high xs q else high xs q
+
+quickSort :: (Ord a) => [a] -> [a]
+quickSort [x] = [x]
+quickSort list = if length b > 0 then quickSort a ++ quickSort b else quickSort (init list) ++ quickSort [last list]
+            where 
+                (a,b) = (low list (last list), high list (last list))
+                
